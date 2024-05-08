4e11333fd1de0b5ab127a50d80fe4d008ba6dac4
Kim ShinWon <itsnotez@gmail.com>
2024-05-08 22:19:02 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
+import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 88, 9, 225),
+                  Color.fromARGB(255, 139, 97, 212)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen(),
+          ),
+        ),
+      ),
+    ),
+  );
 }
